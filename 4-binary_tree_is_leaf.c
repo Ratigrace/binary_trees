41603a5727c975cd037d813cee4e1a7acd84bbf5
Ratigrace <chijongwegrace@gmail.com>
2023-11-01 09:03:30 -0700

    4. Is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - function that checks whether data is a leaf
+ *
+ * @node: pointer to check
+ *
+ * Return: result
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return 0;
+
+	return (node->left == NULL && node->right == NULL);
+}
