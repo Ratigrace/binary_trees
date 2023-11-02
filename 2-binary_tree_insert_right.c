475b84dac740f48ecbf3fd39b6e6f756ded404d5
Ratigrace <chijongwegrace@gmail.com>
2023-11-02 13:42:21 -0700

    Insert left

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,23 +14,21 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
-	if (new_node == NULL)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = parent->right;
-
 	if (parent->right != NULL)
-		parent->right->parent = new_node;
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
 
-	parent->right = new_node;
-
-	return (new_node);
+	return (new);
 }
